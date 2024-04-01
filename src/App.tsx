d36f75ed4cb3722a4450c5db76d57f953f0242fa
Yang Zhao <jenson.zhao@gmail.com>
2024-04-01 07:21:53 -0700

    move flageditor out

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,10 @@
-import { useState, ChangeEvent } from 'react'
+import { useState } from 'react'
+import FlagEditor from './FlagEditor'
 import './App.css'
 
 const flagDefinitionBase = {
   "$schema": "https://flagd.dev/schema/v0/flags.json",
   "flags": {}
-}
-
-function FlagEditor(props: { flagDefinition: any, setFlagDefinition: any }) {
-  const [flagKey, setFlagKey] = useState<string>('')
-  const [state, setState] = useState<boolean>(false)
-
-  const updateFlagKey = (event: ChangeEvent<HTMLInputElement>) => {
-    setFlagKey(oldFlagKey => {
-      const newFlagKey = event.target.value
-      const definition = structuredClone(props.flagDefinition)
-      const flags = definition['flags'] as any
-      if (!oldFlagKey) {
-        flags[newFlagKey] = {
-          "state": state ? 'ENABLED' : 'DISABLED'
-        }
-      } else if (!newFlagKey) {
-        delete flags[oldFlagKey]
-      } else {
-        flags[newFlagKey] = flags[oldFlagKey]
-        delete flags[oldFlagKey]
-      }
-      props.setFlagDefinition(definition)
-      return newFlagKey
-    })
-  }
-
-  const updateState = () => {
-    setState(oldState => {
-      const newState = !oldState
-      const definition = structuredClone(props.flagDefinition)
-      const flags = definition['flags'] as any
-      if (flagKey) {
-        flags[flagKey] = {
-          "state": newState ? 'ENABLED' : 'DISABLED'
-        }
-        props.setFlagDefinition(definition)
-      }
-      return newState
-    })
-  }
-
-  return (
-    <>
-      <div>
-        <div>
-          <label htmlFor='flagKey'>Flag Key</label>
-          <input
-            id='flagKey'
-            value={flagKey}
-            onChange={updateFlagKey} />
-        </div>
-        <div>
-          <label>State</label>
-          <input
-            type='checkbox'
-            checked={state}
-            onChange={updateState} />
-        </div>
-      </div>
-    </>
-  )
 }
 
 function FlagViewer(props: { flagDefinition: any }) {
