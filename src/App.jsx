b5ce4e3ca95d9751de14b61b92e30c9a65dbc6a8
Yang Zhao <jenson.zhao@gmail.com>
2024-04-10 21:30:15 -0700

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css"
 
 function App() {
   const [flagKey, setFlagKey] = useState("")
+  const [state, setState] = useState(false)
 
   const generateJSON = () => {
     const json = {
       flagKey,
+      state,
     }
     return JSON.stringify(json, null, 2)
   }
@@ -18,9 +20,12 @@ function App() {
         <div>
           <label htmlFor="flagKey">FlagKey</label>
           <input id="flagKey" value={flagKey} onChange={(e) => setFlagKey(e.target.value)} />
+          <br />
+          <label htmlFor="state">State</label>
+          <input id="state" type="checkbox" checked={state} onChange={(e) => setState(e.target.checked)} />
         </div>
         <div>
-          <textarea readOnly value={generateJSON()} rows={20} />
+          <textarea readOnly value={generateJSON()} rows={20} cols={50} />
         </div>
       </div>
     </>
