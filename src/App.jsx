e90c23ae4ea8e9784ca82f5de0f3bcf8d8965ce1
Yang Zhao <jenson.zhao@gmail.com>
2024-04-10 21:22:41 -0700

    add flag key

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [flagKey, setFlagKey] = useState("")
+
+  const generateJSON = () => {
+    const json = {
+      flagKey,
+    }
+    return JSON.stringify(json, null, 2)
+  }
 
   return (
     <>
       <h1>flagd ui</h1>
+      <div className="container">
+        <div>
+          <label htmlFor="flagKey">FlagKey</label>
+          <input id="flagKey" value={flagKey} onChange={(e) => setFlagKey(e.target.value)} />
+        </div>
+        <div>
+          <textarea readOnly value={generateJSON()} rows={20} />
+        </div>
+      </div>
     </>
   )
 }
