39524c163cf5707aba337bb4a8ac1f5543f38bb6
Yang Zhao <jenson.zhao@gmail.com>
2024-04-10 21:14:31 -0700

    remove react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import FlagForm from "./FlagForm"
-import FlagViewer from "./FlagViewer"
-import { useForm, FormProvider } from "react-hook-form"
 import "./App.css"
 
 function App() {
-  const methods = useForm()
 
   return (
     <>
       <h1>flagd ui</h1>
-      <FormProvider {...methods}>
-        <div className="container">
-          <FlagForm />
-          <FlagViewer />
-        </div>
-      </FormProvider>
     </>
   )
 }
